01ed5ca9ef0908f743ea6303ab29a1ca0a007e35
gog12345 <travianx00@gmail.com>
2013-02-20 17:57:25 +0200

    Update winner.php

--- winner.php
+++ winner.php
@@ -2,7 +2,7 @@
 ##########################################################################  
 ## -= YOU MAY NOT REMOVE OR CHANGE THIS NOTICE =-##  
 ##  ---------------------------------------------------------------------------   ## 
-##        Filename      all                ##
+##        Filename      winner.php               ##
 ##        Developed by:  gog12345       ##
 ##        License:       TravianS  Project         ##
 ##        Copyright:     TravianS  2013. All rights reserved.    ## 
